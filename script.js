182191a2ce14a1180a2c5d3fd4066c31668a940c
lMertht <valrak.do@gmail.com>
2022-03-30 12:50:14 -0300

    agregados
    @media diagrama
    script user

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
+// USUARIO
+
+function mensajesPedir() {
+
+  let varMensaje = document.querySelector(".imprimirNombres");
+
+  let primerNombre = prompt("primer nombre");
+  let primerApellido = prompt("primer apellido");
+  let suma = primerNombre + " " + primerApellido;
+
+  varMensaje.innerHTML = `Bienvenido ${suma}`;
+
+}
+
+// CONTADOR
 
 const valor = document.querySelector('#valor');
 let contador = 0;
 
-function incrementar () {
-  contador ++;
-  valor.innerHTML = contador;{
-    color ();
+function incrementar() {
+  contador++;
+  valor.innerHTML = contador; {
+    color();
   }
 }
 
-function disminuir () {
-  contador --;
-  valor.innerHTML = contador;{
-    color ();
+function disminuir() {
+  contador--;
+  valor.innerHTML = contador; {
+    color();
   }
-  }
+}
 
-  function resetear () {
-    contador = 0;
-    valor.innerHTML = contador;{
-      color ();
-    }
+function resetear() {
+  contador = 0;
+  valor.innerHTML = contador; {
+    color();
   }
+}
 
-  function color () {
-    if (contador < 0) {
-      valor.style.color = 'red';
-    }
-  else if (contador >= 20){
+function color() {
+  if (contador < 0) {
+    valor.style.color = 'red';
+  } else if (contador >= 20) {
     valor.style.color = 'green';
-  }
-  else {
+  } else {
     valor.style.color = 'white';
   }
-  }
-
-
-  
-
-
-
+}
